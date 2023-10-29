e51cd1edb006bd33ea7d7fab6f0a249a28d0380d
davittsiskarashvili <datotsiskarashvili@gmail.com>
2023-10-29 14:23:02 +0400

    compose project

--- app/src/main/java/com/xmltocompose/FirstFragment.kt
+++ app/src/main/java/com/xmltocompose/FirstFragment.kt
@@ -4,35 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.xmltocompose.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    private val binding get() = _binding!!
+    private lateinit var composeView: ComposeView
+    private val viewModel: FirstViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
-
+        return ComposeView(requireContext()).also {
+            composeView = it
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        composeView.setContent {
+            val state by viewModel.state.collectAsState()
+            FirstScreen(
+                state = state,
+                onNextClick = {
+                    findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+                }
+            )
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
