e5248278c8eb4e52851f0dd8a9271d16732cc359
davittsiskarashvili <datotsiskarashvili@gmail.com>
2023-10-29 14:06:10 +0400

    xml project

--- app/src/main/java/com/xmltocompose/MainActivity.kt
+++ app/src/main/java/com/xmltocompose/MainActivity.kt
@@ -2,10 +2,21 @@ package com.xmltocompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.xmltocompose.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
